160b722a5c3364517504fc287f992932c8dcf0d6
StrPicket <ejohns.ipod@gmail.com>
2026-04-28 13:22:36 -0600

    dataset version 2

--- basic_comms/basic_comms/perception.py
+++ basic_comms/basic_comms/perception.py
@@ -14,8 +14,8 @@ class PuzzlebotAutoCapture(Node):
     def __init__(self):
         super().__init__('puzzlebot_auto_capture')
 
-        self.total_photos = 20
-        self.interval = 2.0
+        self.total_photos = 500
+        self.interval = 1.5
         self.save_dir = "calib_puzzlebot_logitec"
         self.image_topic = "/image_raw"
 
